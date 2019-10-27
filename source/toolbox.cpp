5abf6383c0962547538f10a9a0c69cffc7d51792
J00n <xfxp1384@gmail.com>
2019-10-27 01:21:12 -0700

    02.03.03
    4.85 Support added

--- source/toolbox.cpp
+++ source/toolbox.cpp
@@ -83,7 +83,7 @@ sys_process_param_t __sys_process_param SYS_PROCESS_PARAM_SECTION = {
 
 #define STR_APP_NAME "Rebug Toolbox"
 #define STR_APP_ID	 "RBGTLBOX2"
-#define STR_APP_VER	 "02.03.02"
+#define STR_APP_VER	 "02.03.03"
 
 
 //#include "syscall8.h"
@@ -2835,6 +2835,7 @@ void dump_root_key()
 	if(c_firmware==4.83f && !dex_mode) strcpy(version, "475");   else
 	if(c_firmware==4.83f &&  dex_mode) strcpy(version, "481d");  else
 	if(c_firmware==4.84f && !dex_mode) strcpy(version, "475");   else
+	if(c_firmware==4.85f && !dex_mode) strcpy(version, "475");   else	
 	if(c_firmware==4.84f &&  dex_mode) strcpy(version, "481d");  else	return;
 
 	char rkdumper[64];
@@ -3820,7 +3821,7 @@ int load_texture(u8 *data, char *name, uint16_t dw)
 void pokeq( uint64_t addr, uint64_t val)
 {
 	if(c_firmware!=3.55f && c_firmware!=3.41f && c_firmware!=3.15f && c_firmware!=4.21f && c_firmware!=4.30f && c_firmware!=4.31f && c_firmware!=4.40f && c_firmware!=4.41f && c_firmware!=4.46f && c_firmware!=4.50f && c_firmware!=4.53f &&
-	   c_firmware!=4.55f && c_firmware!=4.60f && c_firmware!=4.65f && c_firmware!=4.66f && c_firmware!=4.70f && c_firmware!=4.75f && c_firmware!=4.76f && c_firmware!=4.78f && c_firmware!=4.80f && c_firmware!=4.81f && c_firmware!=4.82f && c_firmware!=4.83f && c_firmware!=4.84f) return;
+	   c_firmware!=4.55f && c_firmware!=4.60f && c_firmware!=4.65f && c_firmware!=4.66f && c_firmware!=4.70f && c_firmware!=4.75f && c_firmware!=4.76f && c_firmware!=4.78f && c_firmware!=4.80f && c_firmware!=4.81f && c_firmware!=4.82f && c_firmware!=4.83f && c_firmware!=4.84f && c_firmware!=4.85f) return;
 
 	if(!pp_enabled) return;
 	system_call_2(SYSCALL_POKE, addr, val);
@@ -6286,7 +6287,7 @@ void add_utilities()
 	}
 
 	add_xmb_option(xmb[col].member, &xmb[col].size, STR_QA, STR_QADESC,	(char*)"util_qa");
-	if(c_firmware==3.55f || c_firmware==4.21f || c_firmware==4.30f || c_firmware==4.31f || c_firmware==4.40f || c_firmware==4.41f || c_firmware==4.46f  || c_firmware==4.50f || c_firmware==4.53f || c_firmware==4.55f || c_firmware==4.60f || c_firmware==4.65f || c_firmware==4.66f || c_firmware==4.70f || c_firmware==4.75f || c_firmware==4.76f || c_firmware==4.78f || c_firmware==4.80f || c_firmware==4.81f || c_firmware==4.82f || c_firmware==4.83f || c_firmware==4.84f)
+	if(c_firmware==3.55f || c_firmware==4.21f || c_firmware==4.30f || c_firmware==4.31f || c_firmware==4.40f || c_firmware==4.41f || c_firmware==4.46f  || c_firmware==4.50f || c_firmware==4.53f || c_firmware==4.55f || c_firmware==4.60f || c_firmware==4.65f || c_firmware==4.66f || c_firmware==4.70f || c_firmware==4.75f || c_firmware==4.76f || c_firmware==4.78f || c_firmware==4.80f || c_firmware==4.81f || c_firmware==4.82f || c_firmware==4.83f || c_firmware==4.84f || c_firmware==4.85f)
 	{
 		add_xmb_suboption(xmb[col].member[xmb[col].size-1].option, &xmb[col].member[xmb[col].size-1].option_size, 0, (char*)STR_DISABLE,			(char*)"0");
 		add_xmb_suboption(xmb[col].member[xmb[col].size-1].option, &xmb[col].member[xmb[col].size-1].option_size, 0, (char*)STR_ENABLE,				(char*)"1");
@@ -6376,7 +6377,7 @@ void add_utilities()
 	add_xmb_member(xmb[col].member, &xmb[col].size, STR_SAVFLASH, STR_SAVFLASHDESC,
 			/*type*/6, /*status*/2, /*icon*/xmb_icon_tool, 128, 128);
 
-	if( (!dex_mode && (c_firmware==3.55f || c_firmware==4.46f || c_firmware==4.65f || c_firmware==4.66f)) || c_firmware==4.21f || c_firmware==4.70f || c_firmware==4.75f || c_firmware==4.76f || c_firmware==4.78f || c_firmware==4.80f || c_firmware==4.81f || c_firmware==4.82f || c_firmware==4.83f || c_firmware==4.84f)
+	if( (!dex_mode && (c_firmware==3.55f || c_firmware==4.46f || c_firmware==4.65f || c_firmware==4.66f)) || c_firmware==4.21f || c_firmware==4.70f || c_firmware==4.75f || c_firmware==4.76f || c_firmware==4.78f || c_firmware==4.80f || c_firmware==4.81f || c_firmware==4.82f || c_firmware==4.83f || c_firmware==4.84f || c_firmware==4.85f)
 	{
 		add_xmb_member(xmb[col].member, &xmb[col].size, STR_SAVEIDRK, STR_SAVEIDRKDESC,
 			/*type*/6, /*status*/2, /*icon*/xmb_icon_tool, 128, 128);
@@ -6473,7 +6474,7 @@ void add_settings_column()
 		xmb[col].member[xmb[col].size-1].option_selected=menu_mode;
 		xmb[col].member[xmb[col].size-1].icon=xmb_icon_tool;
 
-		if((c_firmware==4.78f || c_firmware==4.80f || c_firmware==4.81f || c_firmware==4.82f || c_firmware==4.83f || c_firmware==4.84f) && cobra_compatible)
+		if((c_firmware==4.78f || c_firmware==4.80f || c_firmware==4.81f || c_firmware==4.82f || c_firmware==4.83f || c_firmware==4.84f || c_firmware==4.85f) && cobra_compatible)
 		{
 			add_xmb_option(xmb[col].member, &xmb[col].size, STR_TOGCFW, STR_TOGCFWDESC,	(char*)"cfw_settings");
 			add_xmb_suboption(xmb[col].member[xmb[col].size-1].option, &xmb[col].member[xmb[col].size-1].option_size, 0, (char*)STR_DISABLE,		(char*)"0");
@@ -6491,7 +6492,7 @@ void add_settings_column()
 			xmb[col].member[xmb[col].size-1].icon=xmb_icon_tool;
 		}*/
 
-		if( dex_mode && (c_firmware==4.76f || c_firmware==4.78f || c_firmware==4.80f || c_firmware==4.81f || c_firmware==4.82f || c_firmware==4.83f || c_firmware==4.84f) && cobra_compatible)
+		if( dex_mode && (c_firmware==4.76f || c_firmware==4.78f || c_firmware==4.80f || c_firmware==4.81f || c_firmware==4.82f || c_firmware==4.83f || c_firmware==4.84f || c_firmware==4.85f) && cobra_compatible)
 		{
 			add_xmb_option(xmb[col].member, &xmb[col].size, (char*)STR_TOGHOSTINF, (char*)STR_TOGHOSTINFDESC,	(char*)"xmb_plugin");
 			add_xmb_suboption(xmb[col].member[xmb[col].size-1].option, &xmb[col].member[xmb[col].size-1].option_size, 0, (char*)STR_DISABLE,		(char*)"0");
@@ -6501,7 +6502,7 @@ void add_settings_column()
 		}
 
 		if((c_firmware==4.21f || c_firmware==4.30f || c_firmware==4.31f || c_firmware==4.40f || c_firmware==4.41f || c_firmware==4.46f || c_firmware==4.50f || c_firmware==4.53f || c_firmware==4.55f ||
-			c_firmware==4.60f || c_firmware==4.65f || c_firmware==4.66f || c_firmware==4.70f || c_firmware==4.75f || c_firmware==4.76f || c_firmware==4.78f || c_firmware==4.80f || c_firmware==4.81f || c_firmware==4.82f || c_firmware==4.83f || c_firmware==4.84f) && cobra_compatible)
+			c_firmware==4.60f || c_firmware==4.65f || c_firmware==4.66f || c_firmware==4.70f || c_firmware==4.75f || c_firmware==4.76f || c_firmware==4.78f || c_firmware==4.80f || c_firmware==4.81f || c_firmware==4.82f || c_firmware==4.83f || c_firmware==4.84f || c_firmware==4.85f) && cobra_compatible)
 		{
 			add_xmb_option(xmb[col].member, &xmb[col].size, STR_TOGCOB, STR_TOGCOBDESC,	(char*)"cobra_mode");
 			add_xmb_suboption(xmb[col].member[xmb[col].size-1].option, &xmb[col].member[xmb[col].size-1].option_size, 0, (char*)STR_DISABLE,			(char*)"0");
@@ -6558,7 +6559,7 @@ void add_settings_column()
 	else if(cobra_compatible)
 	{
 		if((c_firmware==4.21f || c_firmware==4.30f || c_firmware==4.31f || c_firmware==4.40f || c_firmware==4.41f || c_firmware==4.46f || c_firmware==4.50f || c_firmware==4.53f || c_firmware==4.55f ||
-			c_firmware==4.60f || c_firmware==4.65f || c_firmware==4.66f || c_firmware==4.70f || c_firmware==4.75f || c_firmware==4.76f || c_firmware==4.78f || c_firmware==4.80f || c_firmware==4.81f || c_firmware==4.82f || c_firmware==4.83f || c_firmware==4.84f) && cobra_compatible)
+			c_firmware==4.60f || c_firmware==4.65f || c_firmware==4.66f || c_firmware==4.70f || c_firmware==4.75f || c_firmware==4.76f || c_firmware==4.78f || c_firmware==4.80f || c_firmware==4.81f || c_firmware==4.82f || c_firmware==4.83f || c_firmware==4.84f || c_firmware==4.85f) && cobra_compatible)
 		{
 			add_xmb_option(xmb[col].member, &xmb[col].size, STR_TOGCOB, STR_TOGCOBDESC,	(char*)"cobra_mode");
 			add_xmb_suboption(xmb[col].member[xmb[col].size-1].option, &xmb[col].member[xmb[col].size-1].option_size, 0, (char*)STR_DISABLE,			(char*)"0");
@@ -6586,7 +6587,7 @@ void add_settings_column()
 			xmb[col].member[xmb[col].size-1].icon=xmb_icon_tool;
 		}
 
-		if((c_firmware==4.78f || c_firmware==4.80f || c_firmware==4.81f || c_firmware==4.82f || c_firmware==4.83f || c_firmware==4.84f) && cobra_compatible)
+		if((c_firmware==4.78f || c_firmware==4.80f || c_firmware==4.81f || c_firmware==4.82f || c_firmware==4.83f || c_firmware==4.84f || c_firmware==4.85f) && cobra_compatible)
 		{
 			add_xmb_option(xmb[col].member, &xmb[col].size, STR_TOGCFW, STR_TOGCFWDESC,	(char*)"cfw_settings");
 			add_xmb_suboption(xmb[col].member[xmb[col].size-1].option, &xmb[col].member[xmb[col].size-1].option_size, 0, (char*)STR_DISABLE,		(char*)"0");
@@ -8165,13 +8166,13 @@ void write_to_device()
 {
 	if(!exist((char *)"/dev_hdd0/game/RBGTLBOX2/USRDIR/eid_root_key"))
 	{
-		if((c_firmware==4.21f || c_firmware==4.70f || c_firmware==4.75f || c_firmware==4.76f || c_firmware==4.78f || c_firmware==4.80f || c_firmware==4.81f || c_firmware==4.82f || c_firmware==4.83f || c_firmware==4.84f) && dex_mode)
+		if((c_firmware==4.21f || c_firmware==4.70f || c_firmware==4.75f || c_firmware==4.76f || c_firmware==4.78f || c_firmware==4.80f || c_firmware==4.81f || c_firmware==4.82f || c_firmware==4.83f || c_firmware==4.84f || c_firmware==4.85f) && dex_mode)
 		{
 			char message[512];
 			sprintf(message, "%s (%2.2f DEX kernel) %s", STR_WTDMSG1, c_firmware, STR_WTDMSG2);
 			cellMsgDialogOpen2( type_dialog_ok, message, dialog_fun2, (void*)0x0000aaab, NULL );
 		}
-		else if((c_firmware==3.55f || c_firmware==4.21f || c_firmware==4.46f || c_firmware==4.65f || c_firmware==4.66f || c_firmware==4.70f || c_firmware==4.75f || c_firmware==4.76f || c_firmware==4.78f || c_firmware==4.80f || c_firmware==4.81f || c_firmware==4.82f || c_firmware==4.83f || c_firmware==4.84f) && !dex_mode)
+		else if((c_firmware==3.55f || c_firmware==4.21f || c_firmware==4.46f || c_firmware==4.65f || c_firmware==4.66f || c_firmware==4.70f || c_firmware==4.75f || c_firmware==4.76f || c_firmware==4.78f || c_firmware==4.80f || c_firmware==4.81f || c_firmware==4.82f || c_firmware==4.83f || c_firmware==4.84f || c_firmware==4.85f) && !dex_mode)
 		{
 			char message[512];
 			sprintf(message, "%s (%2.2f CEX kernel) %s", STR_WTDMSG1, c_firmware, STR_WTDMSG2);
@@ -8603,6 +8604,8 @@ int main(int argc, char **argv)
 	else
 	if(peekq(0x80000000002ED818ULL)==CEX && peekq(0x80000000002FCB68ULL)==0x323031392F30312FULL) {dex_mode=0; c_firmware=4.84f;} //timestamp: 2019/01
 	else
+	if(peekq(0x80000000002ED818ULL)==CEX && peekq(0x80000000002FCB68ULL)==0x323031392F30372FULL) {dex_mode=0; c_firmware=4.85f;} //timestamp: 2019/07
+	else		
 	if(peekq(0x800000000030F2D0ULL)==DEX && peekq(0x800000000031EF48ULL)==0x323031352F30342FULL) {dex_mode=2; c_firmware=4.75f;} //timestamp: 2015/04
 	else
 	if(peekq(0x800000000030F2D0ULL)==DEX && peekq(0x800000000031EF48ULL)==0x323031352F30382FULL) {dex_mode=2; c_firmware=4.76f;} //timestamp: 2015/08
@@ -8802,7 +8805,7 @@ int main(int argc, char **argv)
 		SYSCALL_TABLE			= SYSCALL_TABLE_470D;
 	}
 	else
-	if((c_firmware==4.75f || c_firmware==4.76f || c_firmware==4.78f || c_firmware==4.81f || c_firmware==4.82f || c_firmware==4.83f || c_firmware==4.84f) && !dex_mode)
+	if((c_firmware==4.75f || c_firmware==4.76f || c_firmware==4.78f || c_firmware==4.81f || c_firmware==4.82f || c_firmware==4.83f || c_firmware==4.84f || c_firmware==4.85f) && !dex_mode)
 	{
 		HVSC_SYSCALL_ADDR		= HVSC_SYSCALL_ADDR_475;
 		NEW_POKE_SYSCALL_ADDR	= NEW_POKE_SYSCALL_ADDR_475;
@@ -9039,7 +9042,7 @@ force_reload:
 				if(xmb[xmb_icon].first==n+5) {export_lv(1);} //lv1
 				if(xmb[xmb_icon].first==n+6) {export_lv(0);} //lv2
 				if(xmb[xmb_icon].first==n+7) {dump_flash();}
-				if((!dex_mode && (c_firmware==3.55f || c_firmware==4.46f || c_firmware==4.65f || c_firmware==4.66f) ) || c_firmware==4.21f  || c_firmware==4.70f || c_firmware==4.75f || c_firmware==4.76f || c_firmware==4.78f || c_firmware==4.80f || c_firmware==4.81f || c_firmware==4.82f || c_firmware==4.83f || c_firmware==4.84f)
+				if((!dex_mode && (c_firmware==3.55f || c_firmware==4.46f || c_firmware==4.65f || c_firmware==4.66f) ) || c_firmware==4.21f  || c_firmware==4.70f || c_firmware==4.75f || c_firmware==4.76f || c_firmware==4.78f || c_firmware==4.80f || c_firmware==4.81f || c_firmware==4.82f || c_firmware==4.83f || c_firmware==4.84f || c_firmware==4.85f)
 				{
 				  if(xmb[xmb_icon].first==n+8) {dump_root_key();}
 				}
@@ -10059,7 +10062,7 @@ void check_settings()
 		if(is_nor() && (cid!=0x82) &&((peek_lv1_cobra(0xF307C) >> 32) == 0x38600001ULL) ) lv1_go=1;
 
 	}
-	else if(c_firmware==4.65f || c_firmware==4.66f || c_firmware==4.70f || c_firmware==4.75f || c_firmware==4.76f || c_firmware==4.78f || c_firmware==4.80f || c_firmware==4.81f || c_firmware==4.82f || c_firmware==4.83f || c_firmware==4.84f) // Fixed
+	else if(c_firmware==4.65f || c_firmware==4.66f || c_firmware==4.70f || c_firmware==4.75f || c_firmware==4.76f || c_firmware==4.78f || c_firmware==4.80f || c_firmware==4.81f || c_firmware==4.82f || c_firmware==4.83f || c_firmware==4.84f || c_firmware==4.85f) // Fixed
 	{
 		if(  peek_lv1_cobra(0x309E4C       ) == 0xE8830018E8840000ULL)	lv1_pp=1;	else lv1_pp=0;    // Fixed   IDA
 		if( (peek_lv1_cobra(0x2b4434) >> 32) == 0x60000000ULL)			lv1_lv2=1;	else lv1_lv2=0;   // Fixed   IDA
@@ -10147,9 +10150,9 @@ void check_settings()
 		cfw_settings=1;	//enabled
 	else if(  (c_firmware==4.78f || c_firmware==4.80f) && exist((char*)"/dev_rebug/vsh/resource/explore/xmb/category_network.xml.cfw") )
 		cfw_settings=0;	//disabled
-	if(  (c_firmware==4.81f || c_firmware==4.82f || c_firmware==4.83f || c_firmware==4.84f) && exist((char*)"/dev_rebug/vsh/resource/explore/xmb/cfw_settings.xml.off") )
+	if(  (c_firmware==4.81f || c_firmware==4.82f || c_firmware==4.83f || c_firmware==4.84f || c_firmware==4.85f) && exist((char*)"/dev_rebug/vsh/resource/explore/xmb/cfw_settings.xml.off") )
 		cfw_settings=1;	//enabled
-	else if( (c_firmware==4.81f || c_firmware==4.82f || c_firmware==4.83f || c_firmware==4.84f) && exist((char*)"/dev_rebug/vsh/resource/explore/xmb/cfw_settings.xml.on") )
+	else if( (c_firmware==4.81f || c_firmware==4.82f || c_firmware==4.83f || c_firmware==4.84f || c_firmware==4.85f) && exist((char*)"/dev_rebug/vsh/resource/explore/xmb/cfw_settings.xml.on") )
 		cfw_settings=0;	//disabled
 	/*if( (c_firmware==4.81f) && exist((char*)"/dev_rebug/vsh/resource/explore/xmb/category_psn.xml.org") )
 		wmlp=1;	//enabled
@@ -10299,7 +10302,7 @@ void change_lv1_um(u8 val)
 		if(val)	poke_lv1(0x0FEB8C, 0x3800000000000000ULL | org);
 		else	poke_lv1(0x0FEB8C, 0xE818000800000000ULL | org);
 	}
-	if(c_firmware==4.50f ||  c_firmware==4.53f ||  c_firmware==4.55f ||  c_firmware==4.60f ||  c_firmware==4.65f ||  c_firmware==4.66f || c_firmware==4.70f || c_firmware==4.75f || c_firmware==4.76f || c_firmware==4.78f || c_firmware==4.80f || c_firmware==4.81f || c_firmware==4.82f || c_firmware==4.83f || c_firmware==4.84f) // Fixed
+	if(c_firmware==4.50f ||  c_firmware==4.53f ||  c_firmware==4.55f ||  c_firmware==4.60f ||  c_firmware==4.65f ||  c_firmware==4.66f || c_firmware==4.70f || c_firmware==4.75f || c_firmware==4.76f || c_firmware==4.78f || c_firmware==4.80f || c_firmware==4.81f || c_firmware==4.82f || c_firmware==4.83f || c_firmware==4.84f || c_firmware==4.85f) // Fixed
 	{
 		u64 org=peek_lv1_cobra(0x0FEBD4) & 0x00000000FFFFFFFFULL;
 		if(val)	poke_lv1(0x0FEBD4, 0x3800000000000000ULL | org);
@@ -10347,7 +10350,7 @@ void change_lv1_dm(u8 val)
 		else	poke_lv1(0x16F800, 0x4800606500000000ULL | org);
 	}
 
-	if(c_firmware==4.30f || c_firmware==4.31f || c_firmware==4.40f || c_firmware==4.41f || c_firmware==4.46f || c_firmware==4.50f || c_firmware==4.53f || c_firmware==4.55f || c_firmware==4.60f || c_firmware==4.65f || c_firmware==4.66f || c_firmware==4.70f || c_firmware==4.75f || c_firmware==4.76f || c_firmware==4.78f || c_firmware==4.80f || c_firmware==4.81f || c_firmware==4.82f || c_firmware==4.83f || c_firmware==4.84f) // Fixed
+	if(c_firmware==4.30f || c_firmware==4.31f || c_firmware==4.40f || c_firmware==4.41f || c_firmware==4.46f || c_firmware==4.50f || c_firmware==4.53f || c_firmware==4.55f || c_firmware==4.60f || c_firmware==4.65f || c_firmware==4.66f || c_firmware==4.70f || c_firmware==4.75f || c_firmware==4.76f || c_firmware==4.78f || c_firmware==4.80f || c_firmware==4.81f || c_firmware==4.82f || c_firmware==4.83f || c_firmware==4.84f || c_firmware==4.85f) // Fixed
 	{
 		u64 org=peek_lv1_cobra(0x16FA64) & 0x00000000FFFFFFFFULL;
 		if(val)	poke_lv1(0x16FA64, 0x6000000000000000ULL | org); //enable patch
@@ -12323,7 +12326,7 @@ void apply_settings(char *option, int val, u8 _forced)
 
 	} // 4.60 Firmware
 
-	if(c_firmware==4.65f || c_firmware==4.66f || c_firmware==4.70f || c_firmware==4.75f || c_firmware==4.76f || c_firmware==4.78f || c_firmware==4.80f || c_firmware==4.81f || c_firmware==4.82f || c_firmware==4.83f || c_firmware==4.84f) // Fixed
+	if(c_firmware==4.65f || c_firmware==4.66f || c_firmware==4.70f || c_firmware==4.75f || c_firmware==4.76f || c_firmware==4.78f || c_firmware==4.80f || c_firmware==4.81f || c_firmware==4.82f || c_firmware==4.83f || c_firmware==4.84f || c_firmware==4.85f) // Fixed
 	{
 		if(!strcmp(option, "lv1_pp"))// || _forced) // Fixed
 		{
@@ -13090,7 +13093,7 @@ void apply_settings(char *option, int val, u8 _forced)
 			//system_call_4(379,0x1200,0,0,0);
 		}
 	}
-	else if((c_firmware==4.81f || c_firmware==4.82f || c_firmware==4.83f || c_firmware==4.84f) && !strcmp(option, "cfw_settings"))
+	else if((c_firmware==4.81f || c_firmware==4.82f || c_firmware==4.83f || c_firmware==4.84f || c_firmware==4.85f) && !strcmp(option, "cfw_settings"))
 	{
 		if( exist((char*)"/dev_rebug/vsh/resource/explore/xmb/cfw_settings.xml.off") )
 			cfw_settings=1;	//enabled
